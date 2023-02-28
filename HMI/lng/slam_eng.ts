a39401de07797e54362438bab1318fa60cdb1211
Marco Nie <github@niekun.net>
2023-02-28 14:57:59 +0800

    增加侧边导航栏按钮

---
+++ HMI/lng/slam_eng.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS>
+	<context>
+		<name>USER_DEF</name>
+		<message>
+			<source>USER</source>
+			<translation>User</translation>
+		</message>
+	</context>
+	<context>
+		<name>SlAmAreaMenu</name>
+		<message>
+			<source>USER</source>
+			<translation>User</translation>
+		</message>
+	</context>
+</TS>
